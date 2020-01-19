9d48ab5594287ad74af6553eb63fa453ec6793a7
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-01-19 09:15:39 -0300

    * Code improvement.

--- includes/functions.php
+++ includes/functions.php
@@ -11,13 +11,20 @@ function buddyforms_cpublishing_get_team_forms() {
 	global $buddyforms;
 
 	$teams = array();
+
+	if ( empty( $buddyforms ) ) {
+		return array();
+	}
+
 	foreach ( $buddyforms as $form_slug => $buddyform ) {
-		if ( isset( $buddyform['form_fields'] ) ) {
+		if ( ! empty( $buddyform['form_fields'] ) ) {
 			foreach ( $buddyform['form_fields'] as $key => $form_field ) {
 				if ( $form_field['type'] == 'collaborative-publishing' ) {
 					$teams[ $form_slug ] = $buddyform['name'];
 				}
 			}
+		} else {
+			return array();
 		}
 	}
 
